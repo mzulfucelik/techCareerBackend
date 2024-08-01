63fc96bc304b853f01a3288615048fed2dda14c1
mzulfucelik <muhammedzulfucelik@gmail.com>
2024-08-01 17:10:35 +0300

    Category Service and Category Controller

---
+++ src/main/java/com/techcareer/todolist/api/CategoryController.java
@@ -0,0 +1,58 @@
+package com.techcareer.todolist.api;
+
+import com.techcareer.todolist.dtos.requests.categories.CategoryAddRequestDto;
+import com.techcareer.todolist.dtos.requests.categories.CategoryUpdateRequestDto;
+import com.techcareer.todolist.entities.Category;
+import com.techcareer.todolist.service.categories.CategoryService;
+import io.swagger.annotations.Api;
+import jakarta.validation.Valid;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("api/categories/")
+@Api(tags = "Kategoriler")
+public class CategoryController {
+
+    private CategoryService categoryService;
+
+    public CategoryController(CategoryService categoryService){
+        this.categoryService = categoryService;
+    }
+
+    @PostMapping("add")
+    public ResponseEntity<String> add(@Valid @RequestBody CategoryAddRequestDto categoryAddRequestDto){
+
+        var result = categoryService.add(categoryAddRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(result);
+
+    }
+
+    @PutMapping("update")
+    public ResponseEntity<String> update(@Valid @RequestBody CategoryUpdateRequestDto updateRequestDto){
+
+        var result = categoryService.update(updateRequestDto);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+    }
+
+    @GetMapping("getbyid")
+    private ResponseEntity<Category> getById(@RequestParam long id){
+
+        var result = categoryService.getById(id);
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+
+    }
+
+    @GetMapping("getall")
+    private ResponseEntity<List<Category>> getAll(){
+
+        var result = categoryService.getAllCategories();
+        return ResponseEntity.status(HttpStatus.OK).body(result);
+
+    }
+
+
+}
--- src/main/java/com/techcareer/todolist/service/categories/CategoryManager.java
+++ src/main/java/com/techcareer/todolist/service/categories/CategoryManager.java
@@ -4,9 +4,12 @@ import com.techcareer.todolist.dataAccess.CategoryRepository;
 import com.techcareer.todolist.dtos.requests.categories.CategoryAddRequestDto;
 import com.techcareer.todolist.dtos.requests.categories.CategoryUpdateRequestDto;
 import com.techcareer.todolist.entities.Category;
+import com.techcareer.todolist.exceptions.NotFoundException;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public final class CategoryManager implements CategoryService{
 
     private final CategoryRepository categoryRepository;
@@ -17,26 +20,78 @@ public final class CategoryManager implements CategoryService{
 
     @Override
     public Category getById(Long id) {
-        return null;
+        Category category = this.categoryRepository.findById(id).orElseThrow(()->new NotFoundException(id,"Kategori"));
+
+        return category;
+
     }
 
     @Override
     public String delete(Long id) {
-        return null;
+
+        Category category = this.categoryRepository.findById(id).orElseThrow(
+                ()->new NotFoundException(id,"Kategori"));
+        this.categoryRepository.delete(category);
+
+        return "Kategori silindi.";
+
     }
 
     @Override
     public List<Category> getAllCategories() {
-        return null;
+
+        return this.categoryRepository.findAll();
+
     }
 
     @Override
     public String add(CategoryAddRequestDto dto) {
-        return null;
+
+        Category category = convertToEntity(dto);
+        this.categoryRepository.save(category);
+
+        return "Kategori eklendi.";
+
     }
 
     @Override
     public String update(CategoryUpdateRequestDto dto) {
-        return null;
+
+        boolean isPresent = this.categoryRepository.existsById(dto.id());
+
+        if (isPresent){
+
+            Category category = convertToEntity(dto);
+            this.categoryRepository.save(category);
+            return "Kategori güncellendi.";
+
+        }
+        else {
+
+
+            throw  new NotFoundException(dto.id(), "Kategori");
+
+        }
+
+    }
+
+    private Category convertToEntity(CategoryAddRequestDto dto){
+
+        Category category = new Category();
+        category.setName(dto.name());
+        category.setDescription(dto.description());
+
+        return category;
+    }
+
+
+    private Category convertToEntity(CategoryUpdateRequestDto dto){
+
+        Category category = new Category();
+        category.setId(dto.id());
+        category.setName(dto.name());
+        category.setDescription(dto.description());
+
+        return category;
     }
 }
